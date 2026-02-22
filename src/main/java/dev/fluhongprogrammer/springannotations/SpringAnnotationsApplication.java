1d579d0664314b3e6151f76e739986dd606d8417
Ivan Besere <apapomsV2@gmail.com>
2026-02-22 23:19:03 +0800

    first commit

---
+++ src/main/java/dev/fluhongprogrammer/springannotations/SpringAnnotationsApplication.java
@@ -0,0 +1,16 @@
+package dev.fluhongprogrammer.springannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnnotationsApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringAnnotationsApplication.class, args);
+		System.out.println("Spring annotations course, let's go!!!!");
+	}
+
+}
